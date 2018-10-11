4e088204ca138e4369491c7d28fc40ac20b423db
digitalfarmer <adesbsp@gmail.com>
2018-10-11 15:59:09 +0700

    menambahkan routing

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var express = require('express'),
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// var routes= require('./routes');
-// routes(app);
+ var routes= require('./routes');
+ routes(app);
 
 app.listen(port);
 console.log('Server Node js Connect Succesfully, start on port :'+port);
